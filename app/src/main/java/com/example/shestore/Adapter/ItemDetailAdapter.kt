28530f98726f457fa0fdbe1e6aeba794b375a0e8
Prateek Singh <prateeksingh3212@gmail.com>
2021-07-17 01:13:15 +0530

    Update Item Detail

---
+++ app/src/main/java/com/example/shestore/Adapter/ItemDetailAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.shestore.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shestore.Model.itemList
+import com.example.shestore.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.cardview_itemdetail_header.view.*
+
+class ItemDetailAdapter(val itemDetail : List<itemList>) : RecyclerView.Adapter<ItemDetailAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemDetailAdapter.ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.cardview_itemdetail_header, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ItemDetailAdapter.ViewHolder, position: Int) {
+        Picasso.get().load(itemDetail[position].imageUrl?.toUri()).into(holder.itemView.cardview_itemDetail_imageview)
+        holder.itemView.cardview_itemDetail_title.text = itemDetail[position].itemName
+        holder.itemView.cardview_itemDetail_subname.text = itemDetail[position].itemSubName
+        holder.itemView.cardview_itemDetail_price.text = itemDetail[position].price.toString()
+    }
+
+    override fun getItemCount(): Int = itemDetail.size
+}
