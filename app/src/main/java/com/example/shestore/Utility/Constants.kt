87a117ace619b5fab2062ae9248c5b5aa34272ac
Prateek Singh <prateeksingh3212@gmail.com>
2021-07-31 23:50:47 +0530

    Enhanced Bottom sheet

--- app/src/main/java/com/example/shestore/Utility/Constants.kt
+++ app/src/main/java/com/example/shestore/Utility/Constants.kt
@@ -3,4 +3,5 @@ package com.example.shestore.Utility
 object Constants {
     const val TRANSITION_TIME = 150L
     const val KEY_ITEM_SLIDE_IMAGE_FRAGMENT_DATA= "itemSlideImage"
+    const val ATTRIBUTE_SIZE = "Size"
 }
